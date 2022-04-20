adad1a79f35fc27a843e0700d7c1267d2f8bf61c
Archan26 <archanparmar99@gmail.com>
2022-04-20 11:58:44 +0530

    Save to hard drive

--- Cards/main.go
+++ Cards/main.go
@@ -3,7 +3,11 @@ package main
 func main() {
 	cards := newDeck()
 
-	hand, remainingDeck := deal(cards, 5)
-	hand.print()
-	remainingDeck.print()
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.print()
+	// remainingDeck.print()
+
+	// fmt.Println(cards.toString())
+
+	cards.saveTofile("my_cards")
 }
