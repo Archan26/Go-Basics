084996c65da0d708a2e676ab629dd753e848b7a5
Archan26 <archanparmar99@gmail.com>
2022-04-21 20:17:37 +0530

    Embedding Structs

--- structs/main.go
+++ structs/main.go
@@ -2,15 +2,33 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipcode int
+}
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var alex person
-	alex.firstName = "Archan"
-	alex.lastName = "Parmar"
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// alex := person{"Alex", "Parmar"}
+	// fmt.Println(alex)
+
+	// var alex person
+	// alex.firstName = "Archan"
+	// alex.lastName = "Parmar"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Archan",
+		lastName:  "Parmar",
+		contact: contactInfo{
+			email:   "abc@gmail.com",
+			zipcode: 334455,
+		},
+	}
+	fmt.Println(jim)
 }
