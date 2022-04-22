22c3911d89f2046c861e68df3aa043b7841e3f4a
Archan26 <archanparmar99@gmail.com>
2022-04-22 08:37:47 +0530

    Basic Receiver in struct

--- structs/main.go
+++ structs/main.go
@@ -10,18 +10,22 @@ type person struct {
 	firstName string
 	lastName  string
 	contact   contactInfo
+	//we can write only "contactInfo"
 }
 
 func main() {
+	//method #1 declaring struct
 	// alex := person{"Alex", "Parmar"}
 	// fmt.Println(alex)
 
+	//method #2 declaring struct
 	// var alex person
 	// alex.firstName = "Archan"
 	// alex.lastName = "Parmar"
 	// fmt.Println(alex)
 	// fmt.Printf("%+v", alex)
 
+	//Embedded struct
 	jim := person{
 		firstName: "Archan",
 		lastName:  "Parmar",
@@ -30,5 +34,9 @@ func main() {
 			zipcode: 334455,
 		},
 	}
-	fmt.Println(jim)
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
