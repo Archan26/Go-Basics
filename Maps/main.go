7a7b0fa066824f49eed5af0a9aab7ec224ea77d9
Archan26 <archanparmar99@gmail.com>
2022-04-22 09:51:23 +0530

    Types of declaration

--- Maps/main.go
+++ Maps/main.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-	}
+	//Method #1
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#00ff00",
+	// }
+
+	//Mehod #2
+	// var colors map[string]string
+
+	//Method #3
+	colors := make(map[string]string)
+
+	colors["White"] = "#ffffff"
 	fmt.Println(colors)
 }
